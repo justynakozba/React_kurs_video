3cee0e7c9342ce9f23f1a5cc56a37c494357fb23
Justyna <justynakozba@gmail.com>
2019-01-28 22:43:22 +0100

    Training application written with use of React library.
    The appication is responsible for adding task
    and count down time to next task.
    The application alows to add newtask with given time.
    Aplication creates task with unique id.
    The delete tag was added as a starting point to delete task.

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import uniqid from "uniqid";
+
+import "./App.css";
+import Countdown from "./Countdown";
+import EditEvent from "./EditEvent";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            events: [
+                { id:0, name:"śniadanie", hour: "07", minute: "00" },
+                { id:1, name:"obiad", hour: "15", minute: "00" },
+                { id:2, name:"kolacja", hour: "19", minute:"00" }
+            ],
+            editedEvents: {
+                id: uniqid(),
+                name:"",
+                hour:"",
+                minute:""
+            }
+        };
+
+        this.handleEditEvent = this.handleEditEvent.bind(this);
+        this.handleSaveEvent = this.handleSaveEvent.bind(this);
+        this.handleRemoveEvent = this.handleRemoveEvent.bind(this);
+    }
+
+    handleEditEvent(val) {
+        this.setState(prevState => {
+            return {
+                editedEvents: Object.assign(prevState.editedEvents, val)
+            };
+        });
+    }
+
+    handleSaveEvent(){
+        this.setState(prevState =>({
+            events: [...prevState.events, prevState.editedEvents],
+            editedEvents: {
+                id:uniqid(),
+                name:"",
+                hour:"",
+                minute:""
+            }
+        }));
+    }
+
+    handleRemoveEvent(){
+
+    }
+    render() {
+        const events = this.state.events.map(el => {
+            return <Countdown key={el.id} name={el.name} hour={el.hour} minute={el.minute}/>;
+        })
+        return (
+        <div className="app">
+            {events}
+            <EditEvent
+                name={this.state.editedEvents.name}
+                hour={this.state.editedEvents.hour}
+                minute={this.state.editedEvents.minute}
+                onInputChange={val => this.handleEditEvent(val)}
+                onSave={() => this.handleSaveEvent() }/>
+        </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Countdown.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Countdown.css";
+import "./../node_modules/semantic-ui-css/semantic.css";
+
+const Countdown = props => (
+    <div className="countdown">
+        <strong>{props.name}</strong> - {props.hour}:{props.minute}
+        <div className="countdown__icons">
+           <i className= "icon times"/>
+        </div>
+    </div>
+);
+
+Countdown.propTypes = {
+    name: PropTypes.string,
+    hour: PropTypes.string,
+    minute: PropTypes.string
+};
+export default Countdown;
